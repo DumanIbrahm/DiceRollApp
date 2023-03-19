25f2034d754b186032e603575848c750a293b9cb
Ibrahim Duman <dumanibrahm@gmail.com>
2023-03-19 16:26:05 +0300

    Son düzenlemeler yapıldı.

--- app/src/main/java/com/example/dicerollapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Button
 import java.util.*
-
 class MainActivity : AppCompatActivity() {
     private lateinit var resultImg1 : ImageView
     private lateinit var resultImg2 : ImageView
@@ -14,37 +13,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         val rollBtn : Button = findViewById(R.id.button)
         resultImg1 = findViewById(R.id.dice1)
         resultImg2 = findViewById(R.id.dice2)
-
         rollBtn.setOnClickListener{
             diceButton()
         }
     }
-    fun diceButton(){
+    private fun diceButton(){
         val dice1Int = Random().nextInt(6) + 1
         val dice2Int = Random().nextInt(6) + 1
-        val imageRes1 = when (dice1Int){
-            1 -> R.drawable.dice1
-            2 -> R.drawable.dice2
-            3 -> R.drawable.dice3
-            4 -> R.drawable.dice4
-            5 -> R.drawable.dice5
-            else -> R.drawable.dice6
-        }
-        val imageRes2 = when (dice2Int){
-            1 -> R.drawable.dice1
-            2 -> R.drawable.dice2
-            3 -> R.drawable.dice3
-            4 -> R.drawable.dice4
-            5 -> R.drawable.dice5
-            else -> R.drawable.dice6
-        }
+        val imageRes1 = imageRes(dice1Int)
+        val imageRes2 = imageRes(dice2Int)
         resultImg1.setImageResource(imageRes1)
         resultImg2.setImageResource(imageRes2)
+    }
+    private fun imageRes(diceInt : Int) : Int{
+        val result = when(diceInt){
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            else -> R.drawable.dice6
+        }
+        return result
     }
 }
 

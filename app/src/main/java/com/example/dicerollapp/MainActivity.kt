e6a01859c78abcc206962ecc38eaa3c238b8b609
Ibrahim Duman <dumanibrahm@gmail.com>
2023-03-24 11:49:44 +0300

    Aynı sayı geldiğinde sağ üstte bir puan görüyoruz.

--- app/src/main/java/com/example/dicerollapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollapp/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var resultImg1 : ImageView
     private lateinit var resultImg2 : ImageView
+    private var imageRes1 : Int = 0
+    private var imageRes2 : Int = 0
+    var point = 0
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,17 +23,24 @@ class MainActivity : AppCompatActivity() {
         val rollBtn : Button = findViewById(R.id.button)
         resultImg1 = findViewById(R.id.dice1)
         resultImg2 = findViewById(R.id.dice2)
+        val scoreText : TextView = findViewById(R.id.textView)
         rollBtn.setOnClickListener{
             diceButton()
+            val toast = Toast.makeText(this,"Dice Roller",Toast.LENGTH_LONG).show()
+            if(imageRes1 == imageRes2){
+                point += 1
+                scoreText.text = "Score : ${point}"
+            }
         }
     }
     private fun diceButton(){
         val dice1Int = Random().nextInt(6) + 1
         val dice2Int = Random().nextInt(6) + 1
-        val imageRes1 = imageRes(dice1Int)
-        val imageRes2 = imageRes(dice2Int)
+         imageRes1 = imageRes(dice1Int)
+         imageRes2 = imageRes(dice2Int)
         resultImg1.setImageResource(imageRes1)
         resultImg2.setImageResource(imageRes2)
+
     }
     private fun imageRes(diceInt : Int) : Int{
         val result = when(diceInt){
@@ -40,4 +54,5 @@ class MainActivity : AppCompatActivity() {
         return result
     }
 }
+
 
